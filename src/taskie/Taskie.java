4752a5cf71b6a3482af6422eef6e7e6602616b95
Yunheng <yun@yun.sg>
2015-02-11 21:51:27 +0800

    Initial Commit

---
+++ src/taskie/Taskie.java
@@ -0,0 +1,15 @@
+package taskie;
+
+public class Taskie {
+	public static void main(String[] args) {
+		try {
+			new Taskie(args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Taskie(String[] args) {
+		
+	}
+}
