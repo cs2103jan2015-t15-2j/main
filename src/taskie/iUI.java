9b4ac439dec465a3960d0c4f8a85313810a73051
whattokingu <keep.it.that.way@gmail.com>
2015-02-12 17:35:28 +0800

    add interface for UI

---
+++ src/taskie/iUI.java
@@ -0,0 +1,8 @@
+package taskie;
+
+public interface iUI {
+	
+	public Boolean printToUser(String message);
+	public Boolean printToUser(String[] messages);
+	public Boolean printToUser(iTask[] TaskList);
+}
