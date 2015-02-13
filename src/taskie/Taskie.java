ab375c348b35e54143374c57b36fc40dc8c6edd6
Yunheng <yun@yun.sg>
2015-02-13 18:45:38 +0800

    Reverse commit 7913a500bc01afe15f680906e9e7bde796b98236

--- src/taskie/Taskie.java
+++ src/taskie/Taskie.java
@@ -1,13 +1,6 @@
 package taskie;
 
 public class Taskie {
-	
-	iTask[] _taskList=null;
-	iStorage _storage=null;
-	iGoogleSync _googleSync=null;
-	iParser _parser= null;
-	iUI _ui=null;
-	
 	public static void main(String[] args) {
 		try {
 			new Taskie(args);
@@ -17,41 +10,5 @@ public class Taskie {
 	}
 	
 	public Taskie(String[] args) {
-		initialise(args);
-		printWelcomeMsg();
-		executeCommandUntilExit();
-		prepareForExit();
-		
-	}
-
-	private void initialise(String[] args) {
-		//initialise global variables
-		
-		initialiseGlobalObjects(args);
-		
-		_storage.setStorageLocation(args[0]);
-		
-		syncWithGoogle();
-		
-		
-	}
-
-	private void syncWithGoogle() {
-		iTask[] taskListFromGoogle=_googleSync.syncFrom();
-		iTask[] taskListFromStorage=_storage.retrieveTaskList();
-		_taskList=syncTask(taskListFromGoogle,taskListFromStorage);
-	}
-
-	private iTask[] syncTask(iTask[] taskListFromGoogle,
-			iTask[] taskListFromStorage) {
-		return null;
-	}
-
-	
-	private void initialiseGlobalObjects(String[] args) {
-		_storage=new Storage();
-		_googleSync = new googleSync();
-		_parser = new parser();
-		_ui=new ui();
 	}
 }
