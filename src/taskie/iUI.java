743440c50efa2499d5c99d82d981b435e4fc1bdb
Yunheng <yun@yun.sg>
2015-02-13 18:55:55 +0800

    Removed UI Interface

--- src/taskie/iUI.java
+++
@@ -1,8 +0,0 @@
-package taskie;
-
-public interface iUI {
-	
-	public Boolean printToUser(String message);
-	public Boolean printToUser(String[] messages);
-	public Boolean printToUser(iTask[] TaskList);
-}
