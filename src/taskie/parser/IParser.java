3ac7a310a6e1a87867c2d6a6065326ae75fdac4a
Yunheng <yun@yun.sg>
2015-02-16 09:01:11 +0800

    Replaced parser interface

--- src/taskie/parser/IParser.java
+++
@@ -1,8 +0,0 @@
-package taskie.parser;
-
-//parser is responsible for taking in input from user. 
-public interface IParser {
-	
-	//parses input to string[]
-	String[] getInput(String userInput);
-}
---
+++ src/taskie/parser/Parser.java
@@ -0,0 +1,6 @@
+//@author A0121555
+package taskie.parser;
+
+public interface Parser {
+	public void parse(String input);
+}
