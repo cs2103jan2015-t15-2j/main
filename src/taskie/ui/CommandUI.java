020cddd7e7e4fec65daca3de9a420f9f81293da6
Yunheng <yun@yun.sg>
2015-02-17 01:06:17 +0800

    Updated Initialization Method & Display String Messages for Command Line UI

--- src/taskie/ui/CommandUI.java
+++ src/taskie/ui/CommandUI.java
@@ -10,19 +10,30 @@ import taskie.parser.Parser;
 
 public class CommandUI implements UI {
 	private static String MESSAGE_WELCOME = "Welcome to Taskie!";
+	private static String MESSAGE_REQUEST_INPUT = "Enter Command: ";
 	
 	private Scanner _scanner;
 	private Parser _parser;
+	private static boolean _isInitialized;
 
 	public CommandUI() {
 		_scanner = new Scanner(System.in);
 		_parser = new CommandParser();
-
-		this.printWelcomeMessage();
-		this.readInput();
+		_isInitialized = false;
+	}
+	
+	public void run() {
+		if ( !_isInitialized ) {
+			_isInitialized = true;
+			this.printWelcomeMessage();
+			while (true) {
+				this.readInput();
+			}
+		}
 	}
 	
 	public void readInput() {
+		System.out.print(MESSAGE_REQUEST_INPUT);
 		String input = _scanner.nextLine();
 		_parser.parse(input);
 	}
@@ -36,7 +47,7 @@ public class CommandUI implements UI {
 	}
 
 	public void display(String message) {
-		
+		System.out.println(message);
 	}
 
 	private void printWelcomeMessage() {
