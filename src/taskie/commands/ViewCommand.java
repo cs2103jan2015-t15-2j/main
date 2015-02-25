64757b3324aca7bf743de51cb4a9711c6ba7474c
Yun Heng <yun@yun.sg>
2015-02-25 22:49:30 +0800

    Added base for search functionality

--- src/taskie/commands/ViewCommand.java
+++ src/taskie/commands/ViewCommand.java
@@ -1,35 +1,101 @@
 package taskie.commands;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import taskie.models.CommandType;
 import taskie.models.ViewType;
 
 public class ViewCommand implements ICommand {
 	private CommandType _commandType = CommandType.VIEW;
-	private ViewType _viewType;
 	
-	private LocalDateTime _startDateTime;
-	private LocalDateTime _endDateTime;
+	//@author A0121555M
+	private ViewType _viewType;
+	private LocalDate _startDate;
+	private LocalDate _endDate;
+	private LocalTime _startTime;
+	private LocalTime _endTime;
+	private String _searchKeywords;
 
 	public ViewCommand(ViewType viewType) {
 		_viewType = viewType;
 	}
 
 	public LocalDateTime getStartDateTime() {
-		return _startDateTime;
+		return LocalDateTime.of(_startDate, (_startTime == null) ? LocalTime.MAX : _startTime);
 	}
-
+	
 	public void setStartDateTime(LocalDateTime startDateTime) {
-		_startDateTime =  startDateTime;
+		this.setStartDate(startDateTime.toLocalDate());
+		this.setStartTime(startDateTime.toLocalTime());
 	}
 
 	public LocalDateTime getEndDateTime() {
-		return _endDateTime;
+		return LocalDateTime.of(_endDate, (_endTime == null) ? LocalTime.MAX : _endTime);
 	}
 
 	public void setEndDateTime(LocalDateTime endDateTime) {
-		_endDateTime =  endDateTime;
+		this.setEndDate(endDateTime.toLocalDate());
+		this.setEndTime(endDateTime.toLocalTime());
+	}
+
+	public LocalDate getStartDate() {
+		return _startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		if ( _endDate == null ) {
+			this._endDate = startDate;
+		}
+		
+		if ( _endDate.isBefore(startDate) ) {
+			this._startDate = this._endDate;
+			this._endDate = startDate;
+		} else {
+			this._startDate = startDate;
+		}
+	}
+
+	public LocalDate getEndDate() {
+		return _endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		if ( _startDate == null ) {
+			this._startDate = endDate;
+		}
+		
+		if ( _startDate.isAfter(endDate) ) {
+			this._endDate = this._startDate;
+			this._startDate = endDate;
+		} else {
+			this._endDate = endDate;
+		}
+	}
+
+	public LocalTime getStartTime() {
+		return _startTime;
+	}
+
+	public void setStartTime(LocalTime startTime) {
+		this._startTime = startTime;
+	}
+
+	public LocalTime getEndTime() {
+		return _endTime;
+	}
+
+	public void setEndTime(LocalTime endTime) {
+		this._endTime = endTime;
+	}
+
+	public String getSearchKeywords() {
+		return _searchKeywords;
+	}
+
+	public void setSearchKeywords(String searchKeywords) {
+		this._searchKeywords = searchKeywords;
 	}
 
 	public CommandType getCommandType() {
