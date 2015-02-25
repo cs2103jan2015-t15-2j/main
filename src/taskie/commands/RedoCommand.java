e56d4930822dbefe8f24b92013d3d8ad89ae9a44
Yun Heng <yun@yun.sg>
2015-02-26 02:34:48 +0800

    Added Redo Command

---
+++ src/taskie/commands/RedoCommand.java
@@ -0,0 +1,22 @@
+//@author A0121555M
+package taskie.commands;
+
+import taskie.models.CommandType;
+
+public class RedoCommand implements ICommand {
+	private CommandType _commandType = CommandType.REDO;
+	private int _steps;
+	
+	public RedoCommand() {
+		this._steps = 1;
+	}
+	
+	public RedoCommand(int steps) {
+		this._steps = steps;
+	}
+
+	@Override
+	public CommandType getCommandType() {
+		return _commandType;
+	}
+}
