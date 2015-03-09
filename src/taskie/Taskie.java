0de407a91468e10ed500a7e77194d8d7f62da9cb
Yun Heng <yun@yun.sg>
2015-03-09 19:00:38 +0800

    Updated main function

--- src/taskie/Taskie.java
+++ src/taskie/Taskie.java
@@ -8,10 +8,7 @@ import taskie.parser.CommandParser;
 import taskie.parser.Parser;
 
 public class Taskie {
-	public static UI UI;
 	public static taskie.controller.Controller Controller;
-	public static IStorage Storage;
-	public static Parser Parser;
 	
 	public static void main(String[] args) {
 		try {
@@ -23,9 +20,6 @@ public class Taskie {
 
 	public Taskie(String[] args) {
 		Controller = new taskie.controller.Controller();
-		Storage = new taskie.database.Storage();
-		UI = new CommandUI();
-		Parser = new CommandParser();
 		Controller.run();
 	}
 }
