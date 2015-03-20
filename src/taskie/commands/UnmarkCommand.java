81c21282983a116125ad5e1f5b0c331d1d3a8373
Yun Heng <yun@yun.sg>
2015-03-20 20:43:36 +0800

    Formatting fixes

--- src/taskie/commands/UnmarkCommand.java
+++ src/taskie/commands/UnmarkCommand.java
@@ -22,12 +22,12 @@ public class UnmarkCommand implements ICommand {
 	private int _taskIndex;
 	private Task _task;
 
-	//@author A0121555M
+	// @author A0121555M
 	public UnmarkCommand(int itemNumber) {
 		_taskIndex = itemNumber;
 	}
-	
-	//@author A0097582N
+
+	// @author A0097582N
 	public UnmarkCommand(Task task) {
 		_task = task;
 	}
@@ -43,35 +43,35 @@ public class UnmarkCommand implements ICommand {
 	@Override
 	public void execute() {
 		try {
-			_task=retrieveTaskFromParser();
-			HashMap<String, ArrayList<Task>> taskLists = Taskie.Controller.getStorage()
-					.retrieveTaskMap();
+			_task = retrieveTaskFromParser();
+			HashMap<String, ArrayList<Task>> taskLists = Taskie.Controller
+					.getStorage().retrieveTaskMap();
 			String taskType = Taskie.Controller.determineTaskType(_task);
-			scanListForTaskAndUnmark(_task, taskLists,taskType);
+			scanListForTaskAndUnmark(_task, taskLists, taskType);
 			Taskie.Controller.getUI().display(formatUnmarkString());
 		} catch (InvalidTaskException e) {
-			Taskie.Controller.getUI().display(taskie.models.Messages.INVALID_TASK_NUM);
+			Taskie.Controller.getUI().display(
+					taskie.models.Messages.INVALID_TASK_NUM);
 		}
 	}
 
 	private void scanListForTaskAndUnmark(Task task,
-			HashMap<String, ArrayList<Task>> taskLists,String taskType) {
-		ArrayList<Task> taskList=taskLists.get(taskType);
-		int taskIndex=taskList.indexOf(task);
-		Task taskRetrieved=taskList.get(taskIndex);
+			HashMap<String, ArrayList<Task>> taskLists, String taskType) {
+		ArrayList<Task> taskList = taskLists.get(taskType);
+		int taskIndex = taskList.indexOf(task);
+		Task taskRetrieved = taskList.get(taskIndex);
 		taskRetrieved.setTaskUndone();
 		Taskie.Controller.getStorage().storeTaskMap(taskLists);
 	}
-	
-	private String formatUnmarkString(){
-		return String.format(taskie.models.Messages.UNMARK_STRING,_task.getTitle());
+
+	private String formatUnmarkString() {
+		return String.format(taskie.models.Messages.UNMARK_STRING,
+				_task.getTitle());
 	}
-	
-	
+
 	private Task retrieveTaskFromParser() throws InvalidTaskException {
-		
+
 		Task task = Taskie.Controller.getUI().getTask(_taskIndex);
 		return task;
-}
-
+	}
 }
