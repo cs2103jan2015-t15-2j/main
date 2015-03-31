b30b1fa1c349d51c00e9dcf6d067f1e64932bc18
Yun Heng <yun@yun.sg>
2015-03-31 17:44:41 +0800

    Closes #93 - log entries now to go temp folder under taskie-log.txt

--- src/taskie/Taskie.java
+++ src/taskie/Taskie.java
@@ -1,9 +1,30 @@
 //@author A0121555M
 package taskie;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Taskie {
+	private static String LOGGER_FILENAME = "taskie-log.txt";
 	public static void main(String[] args) {
+		configureLogger();
 		new Taskie(args);
+	}
+	
+	private static void configureLogger() {
+		FileHandler fh;
+		try {
+			LogManager.getLogManager().reset();
+
+			fh = new FileHandler("%t/" + LOGGER_FILENAME);
+			Logger.getLogger("").addHandler(fh);
+		} catch (SecurityException e) {
+			System.out.println("SecurityException: Unable to setup logging.");
+		} catch (IOException e) {
+			System.out.println("IOException: Unable to setup logging.");
+		}
 	}
 
 	public Taskie(String[] args) {
