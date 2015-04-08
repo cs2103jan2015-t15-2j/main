f04a70997be33e5fc993b0702107bf3afd10b395
whattokingu <keep.it.that.way@gmail.com>
2015-04-08 13:37:09 +0800

    merge conflicts

--- src/taskie/commands/MarkCommand.java
+++ src/taskie/commands/MarkCommand.java
@@ -8,14 +8,8 @@
 
 package taskie.commands;
 
-<<<<<<< HEAD
-import java.util.ArrayList;
 
-import org.apache.commons.lang.ArrayUtils;
-
-=======
 import edu.emory.mathcs.backport.java.util.Arrays;
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 import taskie.exceptions.InvalidTaskException;
 import taskie.exceptions.TaskModificationFailedException;
 import taskie.exceptions.TaskTypeNotSupportedException;
@@ -27,17 +21,7 @@ public class MarkCommand extends AbstractCommand {
 
 	private CommandType _commandType = CommandType.MARK;
 	private Task _task;
-<<<<<<< HEAD
-	private ArrayList<Integer> _taskIndexes;
 
-	//@author A0121555M
-	public MarkCommand(int itemNumber) {
-		_taskIndexes=new ArrayList<Integer>();
-		_taskIndexes.add(itemNumber);
-	}
-	
-
-=======
 	private int[] _taskIndexes;
 
 	//@author A0121555M
@@ -49,14 +33,8 @@ public class MarkCommand extends AbstractCommand {
 		_taskIndexes = itemNumbers;
 	}
 	
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 	//@author A0097582N
-	public MarkCommand(int[] taskIndexes){
-		_taskIndexes = new ArrayList<Integer>();
-		for(int i=0;i<taskIndexes.length;i++){
-			_taskIndexes.add(taskIndexes[i]);
-		}
-	}
+
 	
 	public MarkCommand(Task task) {
 		_task = task;
@@ -71,48 +49,31 @@ public class MarkCommand extends AbstractCommand {
 	}
 
 	public void execute() {
-<<<<<<< HEAD
-		try {
-			while(_taskIndexes.size()>0){
-				_task = retrieveTaskFromUI();
-=======
+
 		for ( int x = 0; x < _taskIndexes.length; x++ ) {
 			try {
 				int index = _taskIndexes[x];
 				
 				_task = retrieveTaskFromUI(index);
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 				Task updatedTask = new Task(_task);
 	
 				if (_task.isDone()) {
 					_controller.getUI().display(DisplayType.ERROR, taskie.models.Messages.TASK_ALREADY_DONE);
 				} else {
 					updatedTask.setTaskDone();
-<<<<<<< HEAD
-				}
-				try {
-					_controller.getStorage().updateTask(_task, updatedTask);
-					_controller.getUI().display(DisplayType.SUCCESS, formatMarkString());
-=======
 					_controller.getUI().display(DisplayType.SUCCESS, formatMarkString());
 				}
 				try {
 					_controller.getStorage().updateTask(_task, updatedTask);
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 				} catch (TaskTypeNotSupportedException e) {
 					_controller.getUI().display(DisplayType.ERROR, e.getMessage());
 				} catch (TaskModificationFailedException e) {
 					_controller.getUI().display(DisplayType.ERROR, e.getMessage());
 				}
-<<<<<<< HEAD
-			}
-		} catch (InvalidTaskException e) {
-				_controller.getUI().display(DisplayType.ERROR,taskie.models.Messages.INVALID_TASK_NUM);
-=======
+
 			} catch (InvalidTaskException e) {
 				_controller.getUI().display(DisplayType.ERROR,taskie.models.Messages.INVALID_TASK_NUM);
 			}
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 		}
 	}
 
@@ -120,34 +81,21 @@ public class MarkCommand extends AbstractCommand {
 		return String.format(taskie.models.Messages.MARK_STRING, _task.getTitle());
 	}
 
-<<<<<<< HEAD
-	private Task retrieveTaskFromUI() throws InvalidTaskException {
-		Task task = _controller.getUI().getTask(_taskIndexes.remove(0));
-=======
+
 	private Task retrieveTaskFromUI(int index) throws InvalidTaskException {
 		Task task = _controller.getUI().getTask(index);
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 		return task;
 	}
 
 	//@author A0121555M
 	public void undo() {
-<<<<<<< HEAD
-		int[] taskIndexes = ArrayUtils.toPrimitive(_taskIndexes.toArray(new Integer[_taskIndexes.size()]));
-		new UnmarkCommand(taskIndexes).execute();
-=======
 		new UnmarkCommand(_taskIndexes).execute();
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 	}
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("CommandType:" + _commandType + ",");
-<<<<<<< HEAD
-		sb.append("TaskIndexes:" + _taskIndexes.toString());
-=======
 		sb.append("TaskIndex:" + Arrays.toString(_taskIndexes));
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 		return sb.toString();
 	}
 }
--- src/taskie/commands/UnmarkCommand.java
+++ src/taskie/commands/UnmarkCommand.java
@@ -8,11 +8,9 @@
 
 package taskie.commands;
 
-
 import java.util.ArrayList;
 
 import org.apache.commons.lang.ArrayUtils;
-
 
 import edu.emory.mathcs.backport.java.util.Arrays;
 
@@ -25,37 +23,23 @@ import taskie.models.Task;
 
 public class UnmarkCommand extends AbstractCommand {
 	private CommandType _commandType = CommandType.MARK;
-<<<<<<< HEAD
-	private ArrayList<Integer> _taskIndexes;
-=======
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
+
 	private Task _task;
 	private int[] _taskIndexes;
 
 	//@author A0121555M
 	public UnmarkCommand(int itemNumber) {
-<<<<<<< HEAD
-		_taskIndexes = new ArrayList<Integer>();
-		_taskIndexes.add(itemNumber);
-=======
+
 		_taskIndexes = new int[] { itemNumber };
 	}
-	
+
 	public UnmarkCommand(int[] itemNumbers) {
 		_taskIndexes = itemNumbers;
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 	}
 
 	//@author A0097582N
 	public UnmarkCommand(Task task) {
 		_task = task;
-	}
-
-	public UnmarkCommand(int[] taskIndexes) {
-		_taskIndexes = new ArrayList<Integer>();
-		for(int i=0;i<taskIndexes.length;i++){
-			_taskIndexes.add(taskIndexes[i]);
-		}
 	}
 
 	public CommandType getCommandType() {
@@ -67,56 +51,53 @@ public class UnmarkCommand extends AbstractCommand {
 	}
 
 	public void execute() {
-<<<<<<< HEAD
-		try {
-			while(_taskIndexes.size()>0){
-				_task = retrieveTaskFromUI();
-=======
-		for ( int x = 0; x < _taskIndexes.length; x++ ) {
+
+		for (int x = 0; x < _taskIndexes.length; x++) {
 			try {
 				int index = _taskIndexes[x];
-				
+
 				_task = retrieveTaskFromUI(index);
->>>>>>> dba536869dfafda0e0a1bc47be929489418e5501
 				Task updatedTask = new Task(_task);
-	
+
 				if (_task.isDone()) {
 					updatedTask.setTaskUndone();
-					_controller.getUI().display(DisplayType.ERROR, formatUnmarkString());
+					_controller.getUI().display(DisplayType.ERROR,
+							formatUnmarkString());
 				} else {
-					_controller.getUI().display(DisplayType.ERROR, taskie.models.Messages.TASK_ALREADY_NOT_DONE);
+					_controller.getUI().display(DisplayType.ERROR,
+							taskie.models.Messages.TASK_ALREADY_NOT_DONE);
 				}
 				try {
 					_controller.getStorage().updateTask(_task, updatedTask);
 				} catch (TaskTypeNotSupportedException e) {
-					_controller.getUI().display(DisplayType.ERROR, e.getMessage());
+					_controller.getUI().display(DisplayType.ERROR,
+							e.getMessage());
 				} catch (TaskModificationFailedException e) {
-					_controller.getUI().display(DisplayType.ERROR, e.getMessage());
+					_controller.getUI().display(DisplayType.ERROR,
+							e.getMessage());
 				}
 
 			} catch (InvalidTaskException e) {
-				_controller.getUI().display(DisplayType.ERROR, taskie.models.Messages.INVALID_TASK_NUM);
+				_controller.getUI().display(DisplayType.ERROR,
+						taskie.models.Messages.INVALID_TASK_NUM);
 
 			}
 		}
 	}
 
 	private String formatUnmarkString() {
-		return String.format(taskie.models.Messages.UNMARK_STRING, _task.getTitle());
+		return String.format(taskie.models.Messages.UNMARK_STRING,
+				_task.getTitle());
 	}
 
-
-	private Task retrieveTaskFromUI() throws InvalidTaskException {
-		Task task = _controller.getUI().getTask(_taskIndexes.remove(0));
-
+	private Task retrieveTaskFromUI(int index) throws InvalidTaskException {
+		Task task = _controller.getUI().getTask(index);
 		return task;
 	}
 
 	//@author A0121555M
 	public void undo() {
-
-		int[] taskIndex = ArrayUtils.toPrimitive(_taskIndexes.toArray(new Integer[_taskIndexes.size()]));
-		new MarkCommand(taskIndex).execute();
+		new MarkCommand(_taskIndexes).execute();
 
 	}
 
@@ -124,7 +105,7 @@ public class UnmarkCommand extends AbstractCommand {
 		StringBuilder sb = new StringBuilder();
 		sb.append("CommandType:" + _commandType + ",");
 
-		sb.append("TaskIndexes:" + _taskIndexes.toString());
+		sb.append("TaskIndexes:" + Arrays.toString(_taskIndexes));
 		return sb.toString();
 	}
 }
