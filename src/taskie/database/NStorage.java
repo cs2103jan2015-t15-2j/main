74d277e22592fadbf527a7e2de908b84bdce26c5
Yun Heng <yun@yun.sg>
2015-04-11 18:14:57 +0800

    organized imports

--- src/taskie/database/NStorage.java
+++ src/taskie/database/NStorage.java
@@ -11,7 +11,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import taskie.exceptions.FileExistsException;
-import taskie.exceptions.InvalidCommandException;
 import taskie.exceptions.StorageLocationInvalidException;
 import taskie.exceptions.StorageMigrationFailedException;
 import taskie.exceptions.TaskModificationFailedException;
