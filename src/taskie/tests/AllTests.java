7b2abb18f7b2de7e7713fa196411e5e06f0cbe8d
whattokingu <keep.it.that.way@gmail.com>
2015-04-16 18:34:40 +0800

    slight modification to test suite

--- src/taskie/tests/AllTests.java
+++ src/taskie/tests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ CommandTest.class, FunctionalTest.class, ModelTest.class,
+@SuiteClasses({ FunctionalTest.class, CommandTest.class, ModelTest.class,
 		ParserTest.class, StorageTest.class })
 public class AllTests {
 
